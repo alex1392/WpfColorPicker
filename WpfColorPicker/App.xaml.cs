5f64c55992b19acaa1d9ff4ce0112f77fb81c53a
Alex Chuang <1392alex1392@gmail.com>
2019-02-12 17:44:39 +0000

    add globalization

--- WpfColorPicker/App.xaml.cs
+++ WpfColorPicker/App.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using CycWpfLibrary;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +15,43 @@ namespace WpfColorPicker
   /// </summary>
   public partial class App : Application
   {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+      base.OnStartup(e);
+      LoadLanguage();
+    }
+
+    private Uri defaultUri = new Uri(@"pack://application:,,,/WpfColorPicker;component/lang/default.xaml", UriKind.Absolute);
+    private void LoadLanguage()
+    {
+      CultureInfo currentCultureInfo = CultureInfo.CurrentCulture;
+
+      ResourceDictionary rd = null;
+
+      try
+      {
+        rd = LoadComponent(new Uri( @"lang/" + currentCultureInfo.Name + ".xaml", UriKind.Relative)) as ResourceDictionary;
+      }
+      catch
+      {
+      }
+
+      if (rd != null)
+      {
+        if (this.Resources.MergedDictionaries.Count > 0)
+        {
+          foreach (var d in Resources.MergedDictionaries)
+          {
+            if (d.Source == defaultUri)
+            {
+              d.Clear();
+              break;
+            }
+          }
+          
+        }
+        this.Resources.MergedDictionaries.Add(rd);
+      }
+    }
   }
 }
--- WpfColorPicker/MainWindow.xaml.cs
+++ WpfColorPicker/MainWindow.xaml.cs
@@ -1,65 +1,81 @@
 ﻿using CycWpfLibrary;
+using CycWpfLibrary.Emgu;
+using CycWpfLibrary.Media;
 using CycWpfLibrary.MVVM;
 using CycWpfLibrary.WinForm;
+using Emgu.CV;
+using Emgu.CV.Structure;
 using System;
+using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using PointWF = System.Drawing.Point;
 
 namespace WpfColorPicker
 {
-
   /// <summary>
   /// MainWindow.xaml 的互動邏輯
   /// </summary>
   public partial class MainWindow : ObservableWindow
   {
-    [DllImport("user32.dll")]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    static extern bool GetCursorPos(ref PointWF lpPoint);
-
     public MainWindow()
     {
       InitializeComponent();
       gridMain.DataContext = this;
-      
-      new Thread(() =>
+
+      //Task.Run(async () =>
+      //{
+      //  while (true)
+      //  {
+      //    PointWF posWF = new PointWF();
+      //    ScreenMethods.GetCursorPos(ref posWF);
+      //    var posWpf = posWF.ToWpf();
+      //    await Dispatcher.BeginInvoke(new Action(() =>
+      //    {
+      //      ImageSource = ScreenMethods.CopyScreen(new Rect(posWpf.Minus(length / 2), posWpf.Add(length / 2)));
+      //    }));
+
+      //    await Task.Delay(100);
+      //  }
+      //});
+
+      Task.Run(() =>
       {
         while (true)
         {
-          //Logic
-          PointWF p = new PointWF();
-          GetCursorPos(ref p);
-          var pos = p.ToWpf();
-          //Update UI
+          PointWF posWF = new PointWF();
+          ScreenMethods.GetCursorPos(ref posWF);
+          var posWpf = posWF.ToWpf();
           Dispatcher.BeginInvoke(new Action(() =>
           {
-            ImageSource = ScreenExtensions.CopyScreen(new Rect(pos.Minus(length / 2), pos.Add(length / 2)));
+            Image = ScreenMethods.CopyScreen(new Rect(posWpf.Minus(length / 2), posWpf.Add(length / 2)));
           }));
 
-          Thread.Sleep(100);
+          Thread.Sleep(50);
         }
-      }).Start();
+      });
     }
 
     private int length = 30;
 
-    public ImageSource ImageSource { get; set; }
+    public BitmapSource ImageSource => Image?.ToBitmapSource();
 
-    private void Window_MouseMove(object sender, MouseEventArgs e)
-    {
-      //{
-      //  var pos = this.GetMouseScreenPos();
-      //  ImageSource = ScreenExtensions.CopyScreen(new Rect(pos.Minus(length / 2), pos.Add(length / 2)));
-      //}
-    }
+    public Image<Bgra, byte> Image { get; set; }
 
-    private void ObservableWindow_Activated(object sender, EventArgs e)
-    {
-      //Mouse.Capture(this);
-    }
+    public byte B => Image?.Data[Image.Width / 2, Image.Height / 2, 0] ?? 0;
+    public byte G => Image?.Data[Image.Width / 2, Image.Height / 2, 1] ?? 0;
+    public byte R => Image?.Data[Image.Width / 2, Image.Height / 2, 2] ?? 0;
+    public byte A => Image?.Data[Image.Width / 2, Image.Height / 2, 3] ?? 0;
+
+    public string Bstring => "B = " + B.ToString();
+    public string Gstring => "G = " + G.ToString();
+    public string Rstring => "R = " + R.ToString();
+    public string Astring => "A = " + A.ToString();
+    public string HexString => new byte[] { A, R, G, B }.ToHexString();
   }
 }
