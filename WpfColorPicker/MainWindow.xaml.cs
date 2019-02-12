fdd30641cafedba4657cb31bdcc35225794e6863
Alex Chuang <1392alex1392@gmail.com>
2019-02-12 07:43:59 +0000

    Complete screen snapshot

--- WpfColorPicker/MainWindow.xaml.cs
+++ WpfColorPicker/MainWindow.xaml.cs
@@ -1,28 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CycWpfLibrary;
+using CycWpfLibrary.MVVM;
+using CycWpfLibrary.WinForm;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using PointWF = System.Drawing.Point;
 
 namespace WpfColorPicker
 {
+
   /// <summary>
   /// MainWindow.xaml 的互動邏輯
   /// </summary>
-  public partial class MainWindow : Window
+  public partial class MainWindow : ObservableWindow
   {
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    static extern bool GetCursorPos(ref PointWF lpPoint);
+
     public MainWindow()
     {
       InitializeComponent();
+      gridMain.DataContext = this;
+      
+      new Thread(() =>
+      {
+        while (true)
+        {
+          //Logic
+          PointWF p = new PointWF();
+          GetCursorPos(ref p);
+          var pos = p.ToWpf();
+          //Update UI
+          Dispatcher.BeginInvoke(new Action(() =>
+          {
+            ImageSource = ScreenExtensions.CopyScreen(new Rect(pos.Minus(length / 2), pos.Add(length / 2)));
+          }));
+
+          Thread.Sleep(100);
+        }
+      }).Start();
+    }
+
+    private int length = 30;
+
+    public ImageSource ImageSource { get; set; }
+
+    private void Window_MouseMove(object sender, MouseEventArgs e)
+    {
+      //{
+      //  var pos = this.GetMouseScreenPos();
+      //  ImageSource = ScreenExtensions.CopyScreen(new Rect(pos.Minus(length / 2), pos.Add(length / 2)));
+      //}
+    }
+
+    private void ObservableWindow_Activated(object sender, EventArgs e)
+    {
+      //Mouse.Capture(this);
     }
   }
 }
