5891d3c420c5b763026ff405e1266dda13b41a54
Alex Chuang <1392alex1392@gmail.com>
2019-02-19 06:00:34 +0000

    improve display, complete color picker functionality

--- WpfColorPicker/MainWindow.xaml.cs
+++ WpfColorPicker/MainWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using CycWpfLibrary;
 using CycWpfLibrary.Emgu;
 using CycWpfLibrary.Media;
-using CycWpfLibrary.MVVM;
-using CycWpfLibrary.WinForm;
 using Emgu.CV;
 using Emgu.CV.Structure;
 using System;
@@ -14,10 +12,18 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using ColorWF = System.Drawing.Color;
+using ColorWpf = System.Windows.Media.Color;
 using PointWF = System.Drawing.Point;
+using PointWpf = System.Windows.Point;
 
 namespace WpfColorPicker
 {
+  public enum AppState
+  {
+    Dropped, // color fixed
+    Dropping, //update colors
+  }
   /// <summary>
   /// MainWindow.xaml 的互動邏輯
   /// </summary>
@@ -28,54 +34,99 @@ namespace WpfColorPicker
       InitializeComponent();
       gridMain.DataContext = this;
 
-      //Task.Run(async () =>
-      //{
-      //  while (true)
-      //  {
-      //    PointWF posWF = new PointWF();
-      //    ScreenMethods.GetCursorPos(ref posWF);
-      //    var posWpf = posWF.ToWpf();
-      //    await Dispatcher.BeginInvoke(new Action(() =>
-      //    {
-      //      ImageSource = ScreenMethods.CopyScreen(new Rect(posWpf.Minus(length / 2), posWpf.Add(length / 2)));
-      //    }));
-
-      //    await Task.Delay(100);
-      //  }
-      //});
-
       Task.Run(() =>
       {
         while (true)
         {
           PointWF posWF = new PointWF();
           ScreenMethods.GetCursorPos(ref posWF);
-          var posWpf = posWF.ToWpf();
-          Dispatcher.BeginInvoke(new Action(() =>
-          {
-            Image = ScreenMethods.CopyScreen(new Rect(posWpf.Minus(length / 2), posWpf.Add(length / 2)));
-          }));
+          CursorPos = posWF.ToWpf();
+          Dispatcher.BeginInvoke(new Action(() => Update()));
 
           Thread.Sleep(50);
         }
       });
     }
 
-    private int length = 30;
+    private void mainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+      State = AppState.Dropping;
+    }
 
-    public BitmapSource ImageSource => Image?.ToBitmapSource();
+    private const int length = 30;
+    private const int halfLength = length / 2;
+
+    public PointWpf CursorPos { get; set; }
 
     public Image<Bgra, byte> Image { get; set; }
+    public BitmapSource ImageSource => Image?.ToBitmapSource();
 
-    public byte B => Image?.Data[Image.Width / 2, Image.Height / 2, 0] ?? 0;
-    public byte G => Image?.Data[Image.Width / 2, Image.Height / 2, 1] ?? 0;
-    public byte R => Image?.Data[Image.Width / 2, Image.Height / 2, 2] ?? 0;
-    public byte A => Image?.Data[Image.Width / 2, Image.Height / 2, 3] ?? 0;
+    public byte B { get; set; }
+    public byte G { get; set; }
+    public byte R { get; set; }
+    public byte A { get; set; }
+    public SolidColorBrush ColorBrush { get; set; }
+    public string HexString { get; set; }
 
-    public string Bstring => "B = " + B.ToString();
-    public string Gstring => "G = " + G.ToString();
-    public string Rstring => "R = " + R.ToString();
-    public string Astring => "A = " + A.ToString();
-    public string HexString => new byte[] { A, R, G, B }.ToHexString();
+    private void Update()
+    {
+      Image = ScreenMethods.CopyScreen(new Rect(CursorPos.Minus(halfLength), CursorPos.Add(halfLength)));
+      if (State == AppState.Dropped)
+        return;
+      var data = Image.Data;
+      B = data[halfLength, halfLength, 0];
+      G = data[halfLength, halfLength, 1];
+      R = data[halfLength, halfLength, 2];
+      A = data[halfLength, halfLength, 3];
+      ColorBrush = new SolidColorBrush(ColorWpf.FromArgb(A, R, G, B));
+      HexString = new byte[] { A, R, G, B }.ToHexString();
+    }
+
+    private AppState state = default(AppState);
+    public AppState State
+    {
+      get => state;
+      set
+      {
+        state = value;
+        switch (state)
+        {
+          case AppState.Dropped:
+            dropperButton.IsEnabled = true;
+            ReleaseMouseCapture();
+            Cursor = Cursors.AppStarting;
+            break;
+          case AppState.Dropping:
+            dropperButton.IsEnabled = false;
+            CaptureMouse();
+            Cursor = cursorDropper;
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    private static readonly Cursor cursorDropper = new Bitmap(Application.GetResourceStream(new Uri("resources/dropper.png", UriKind.RelativeOrAbsolute)).Stream).ToCursor(new PointWpf(0.5, 0.5));
+    private void dropperButton_Click(object sender, RoutedEventArgs e)
+    {
+      State = AppState.Dropping; // Dropped -> Dropping      
+    }
+
+    private void mainWindow_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+    }
+
+    private void mainWindow_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      if (State == AppState.Dropping)
+      {
+        State = AppState.Dropped; // Dropping -> Dropped
+      }
+
+    }
+
+    private void mainWindow_MouseLeave(object sender, MouseEventArgs e)
+    {
+    }
   }
 }
